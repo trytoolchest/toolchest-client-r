d6cd525f54f866fd8cb1d40cfcac47ff1e4894d6
bcai2 <bcai@alumni.stanford.edu>
2021-07-08 08:10:47 -0500

    added bowtie2 and test tools

--- R/tools.R
+++ R/tools.R
@@ -1,3 +1,23 @@
+#' Bowtie 2 Client
+#'
+#' Starts a query for Bowtie 2 (for alignment) using Toolchest.
+#'
+#' If left unspecified, inputs and output_path can be selected by the user
+#' manually.
+#'
+#' @param inputs: Path or list of paths (client-side) to be passed in as input.
+#' @param output_path: Path (client-side) where the output file will be downloaded.
+#' @param database_name Name (string) of database to use for Bowtie 2 alignment.
+#' @param database_version Version (string) of database to use for Bowtie 2 alignment.
+#' @param tool_args (optional) Additional arguments to be passed to Bowtie 2.
+#'
+#' @export
+bowtie2 <- function(inputs = NULL, output_path = NULL, database_name, database_version, tool_args="") {
+  inputs <- .validate.inpath(inputs)
+  output_path <- .validate.outpath(output_path)
+  toolchest_client$bowtie2(inputs, output_path, database_name, database_version, tool_args)
+}
+
 #' Cutadapt Client
 #'
 #' Starts a query for Cutadapt using Toolchest.
@@ -38,7 +58,6 @@ cutadapt <- function(inputs = NULL, output_path = NULL, tool_args) {
 #' @param output_path Path (client-side) where the output will be downloaded.
 #' @param tool_args (optional) Additional arguments to be passed to Kraken 2.
 #'
-#'
 #' @export
 kraken2 <- function(inputs = NULL, output_path = NULL, tool_args = "") {
   inputs <- .validate.inpath(inputs)
@@ -46,3 +65,17 @@ kraken2 <- function(inputs = NULL, output_path = NULL, tool_args = "") {
   toolchest_client$kraken2(inputs, output_path, tool_args)
 }
 
+#' Test Pipeline Segment
+#'
+#' Run a test pipeline segment via Toolchest. A plain text file containing 'success' is returned."
+#'
+#' @param tool_args Additional arguments, present to maintain a consistent interface. This is disregarded.
+#' @param inputs Path or list of paths (client-side) to be passed in as input.
+#' @param output_path Path (client-side) where the output file will be downloaded.
+#'
+#' @export
+test <- function(inputs = NULL, output_path = NULL, tool_args = "") {
+  inputs <- .validate.inpath(inputs)
+  output_path <- .validate.outpath(output_path)
+  toolchest_client$test(inputs, output_path, tool_args)
+}
