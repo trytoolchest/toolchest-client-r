443f464e3e8d5879d6dba9b49dc8694afd286611
bcai2 <bcai@alumni.stanford.edu>
2021-07-09 17:37:03 -0500

    changed tool_args placement in tool function argument order

--- R/tools.R
+++ R/tools.R
@@ -5,14 +5,14 @@
 #' If left unspecified, inputs and output_path can be selected by the user
 #' manually.
 #'
+#' @param tool_args (optional) Additional arguments to be passed to Bowtie 2.
 #' @param inputs Path or list of paths (client-side) to be passed in as input.
 #' @param output_path Path (client-side) where the output file will be downloaded.
 #' @param database_name Name (string) of database to use for Bowtie 2 alignment.
 #' @param database_version Version (string) of database to use for Bowtie 2 alignment.
-#' @param tool_args (optional) Additional arguments to be passed to Bowtie 2.
 #'
 #' @export
-bowtie2 <- function(inputs = NULL, output_path = NULL, database_name, database_version, tool_args = "") {
+bowtie2 <- function(tool_args = "", inputs = NULL, output_path = NULL, database_name, database_version) {
   inputs <- .validate.inpath(inputs)
   output_path <- .validate.outpath(output_path)
   toolchest_client$bowtie2(inputs, output_path, database_name, database_version, tool_args)
@@ -28,9 +28,9 @@ bowtie2 <- function(inputs = NULL, output_path = NULL, database_name, database_v
 #' If left unspecified, inputs and output_path can be selected by the user
 #' manually.
 #'
+#' @param tool_args Additional arguments to be passed to Cutadapt.
 #' @param inputs Path or list of paths (client-side) to be passed in as input.
 #' @param output_path Path (client-side) where the output file will be downloaded.
-#' @param tool_args Additional arguments to be passed to Cutadapt.
 #'
 #' @examples
 #' \dontrun{
@@ -39,7 +39,7 @@ bowtie2 <- function(inputs = NULL, output_path = NULL, database_name, database_v
 #' }
 #'
 #' @export
-cutadapt <- function(inputs = NULL, output_path = NULL, tool_args) {
+cutadapt <- function(tool_args, inputs = NULL, output_path = NULL) {
   inputs <- .validate.inpath(inputs)
   output_path <- .validate.outpath(output_path)
   toolchest_client$cutadapt(inputs, output_path, tool_args)
@@ -54,12 +54,12 @@ cutadapt <- function(inputs = NULL, output_path = NULL, tool_args) {
 #' If left unspecified, inputs and output_path can be selected by the user
 #' manually.
 #'
+#' @param tool_args (optional) Additional arguments to be passed to Kraken 2.
 #' @param inputs Path or list of paths (client-side) to be passed in as input.
 #' @param output_path Path (client-side) where the output will be downloaded.
-#' @param tool_args (optional) Additional arguments to be passed to Kraken 2.
 #'
 #' @export
-kraken2 <- function(inputs = NULL, output_path = NULL, tool_args = "") {
+kraken2 <- function(tool_args = "", inputs = NULL, output_path = NULL) {
   inputs <- .validate.inpath(inputs)
   output_path <- .validate.outpath(output_path)
   toolchest_client$kraken2(inputs, output_path, tool_args)
@@ -69,12 +69,12 @@ kraken2 <- function(inputs = NULL, output_path = NULL, tool_args = "") {
 #'
 #' Run a test pipeline segment via Toolchest. A plain text file containing 'success' is returned."
 #'
+#' @param tool_args Additional arguments, present to maintain a consistent interface. This is disregarded.
 #' @param inputs Path or list of paths (client-side) to be passed in as input.
 #' @param output_path Path (client-side) where the output file will be downloaded.
-#' @param tool_args Additional arguments, present to maintain a consistent interface. This is disregarded.
 #'
 #' @export
-test <- function(inputs = NULL, output_path = NULL, tool_args = "") {
+test <- function(tool_args = "", inputs = NULL, output_path = NULL) {
   inputs <- .validate.inpath(inputs)
   output_path <- .validate.outpath(output_path)
   toolchest_client$test(inputs, output_path, tool_args)
