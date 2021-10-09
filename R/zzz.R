672504f9bc85e1a9fae33e98a635512f40f3d6a4
Bryce Cai <bryce.cai2@gmail.com>
2021-10-08 17:34:47 -0700

    `pysam` install fix (#10)
    Resolves the R client's issues with installing `pysam`  by enforcing:
    * `setuptools` version `58.0.0` in the `python` env
    * `libpython` libraries within the `python` env used.
     
    If necessary, reinstalls `setuptools`, re-creates the `r-reticulate` env, and/or prompts the user to install a proper version of `python` (`3.6` or greater, with `libpython` libraries).
    
    Prevents the `r-reticulate` virtual env from being re-created if it already exists and proper versions of `python` and `setuptools` are detected.

--- R/zzz.R
+++ R/zzz.R
@@ -1,37 +1,7 @@
-#' Installs and loads the `toolchest_client` Python package when loading
-#' the R package.
-#'
-#' Note: The Python package interface is stored in the global variable `toolchest_client`.
-#'
-#'
 .onLoad <- function(libname, pkgname) {
-  packageStartupMessage("Installing Toolchest client... ")
+  startup_message <- paste("Toolchest client loaded.")
+  packageStartupMessage(startup_message)
 
-  packageStartupMessage("Configuring reticulate...")
-  reticulate::virtualenv_create("r-reticulate")
-  reticulate::virtualenv_install(
-    envname = "r-reticulate",
-    packages = "toolchest_client",
-    ignore_installed = TRUE
-  )
-
-  reticulate::configure_environment("toolchest_client")
-  toolchest_client <<- reticulate::import("toolchest_client", delay_load = TRUE)
-
-  packageStartupMessage("The Toolchest client has been installed!")
-
-  # Detect if TOOLCHEST_KEY is specified in .Renviron or elsewhere.
-  # Notify user if this is not the case.
-  config_message <- ""
-  if (Sys.getenv("TOOLCHEST_KEY") == "") {
-    config_message <- paste(
-      "To use Toolchest, please set your given key with:",
-      "",
-      "  toolchest::set_key(\"YOUR_TOOLCHEST_KEY\")",
-      "",
-      "If you do not have a key, please contact Toolchest.",
-      sep = "\n"
-    )
-  }
-  packageStartupMessage(config_message)
+  # Install Toolchest Python package on package load.
+  install_toolchest()
 }
