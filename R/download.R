de323492b817d70bc157d0c80f7d40cf6643e598
Bryce Cai <bcai@alumni.stanford.edu>
2022-02-07 10:56:38 -0800

    feat: manual download support (#18)
    Adds:
    - Support for manual downloads via `toolchest::download()`.
    - Support for skipping downloads for `toolchest::tool()` calls if `ouptut_path` is omitted.
    - Support for invoking downloads directly via an `output` (see below) with `output$download(output_path)`.
    
    Modifies:
    - `toolchest::tool()` calls now return an `output`, which contains data on the output's location in the cloud (`output$s3_uri`) and in local storage (`output$output_path`) if downloaded.

---
+++ R/download.R
@@ -0,0 +1,32 @@
+#' Download
+#'
+#' Downloads the output of a previously run job.
+#'
+#' One of `s3_uri` or `pipeline_segment_instance_id` must be provided.
+#'
+#' @param output_path Output path to which the file(s) will be downloaded.
+#' This should be a directory that already exists, but direct filenames
+#' are also supported.
+#' @param s3_uri URI of file contained in S3. This can be passed from
+#' the parameter `output$s3_uri` from the `output` returned by a previous
+#' job.
+#' @param pipeline_segment_instance_id Pipeline segment instance ID of the job
+#' producing the output you would like to download.
+#' @return Path(s) to downloaded and decompressed files. If `skip_decompression` is enabled,
+#' returns the path to the archive.
+#'
+#' @note The `download` function can also be invoked from the output of any
+#' `toolchest::tool()` function call as `output$download(output_path)`. With an
+#' output, only the path to the local directory needs to be specified.
+#'
+#' @export
+download <- function(output_path, s3_uri = NULL, pipeline_segment_instance_id = NULL) {
+  output_path <- .validate.outpath(output_path)
+  toolchest_args <- list(
+    output_path = output_path,
+    s3_uri = s3_uri,
+    pipeline_segment_instance_id = pipeline_segment_instance_id
+  )
+  output <- .do.toolchest.call(toolchest_client$download, toolchest_args)
+  return(output)
+}
